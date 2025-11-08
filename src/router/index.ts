8a431d90c2ff27d177a6b639173b2cc3a0326f41
fakhrihabb <fakhrihabb@gmail.com>
2025-11-08 17:21:05 +0700

    feat: configure Vue Router with all application routes
    - Add property routes: list, create, detail, update, add room
    - Add booking routes: list, create, detail, update (with room parameter)
    - Add statistics route for income analytics
    - Configure lazy loading for optimized code splitting

--- src/router/index.ts
+++ src/router/index.ts
@@ -9,16 +9,59 @@ const router = createRouter({
       name: 'dashboard',
       component: DashboardView,
     },
+    // Property routes
     {
       path: '/property',
       name: 'property',
       component: () => import('../views/PropertyView.vue'),
     },
     {
+      path: '/property/create',
+      name: 'property-create',
+      component: () => import('../views/PropertyCreateView.vue'),
+    },
+    {
+      path: '/property/:id',
+      name: 'property-detail',
+      component: () => import('../views/PropertyDetailView.vue'),
+    },
+    {
+      path: '/property/update/:id',
+      name: 'property-update',
+      component: () => import('../views/PropertyUpdateView.vue'),
+    },
+    {
+      path: '/property/updateroom/:id',
+      name: 'property-add-room',
+      component: () => import('../views/PropertyAddRoomView.vue'),
+    },
+    // Booking routes
+    {
       path: '/bookings',
       name: 'bookings',
       component: () => import('../views/BookingsView.vue'),
     },
+    {
+      path: '/bookings/create',
+      name: 'booking-create',
+      component: () => import('../views/BookingCreateView.vue'),
+    },
+    {
+      path: '/bookings/create/:roomId',
+      name: 'booking-create-room',
+      component: () => import('../views/BookingCreateView.vue'),
+    },
+    {
+      path: '/bookings/:id',
+      name: 'booking-detail',
+      component: () => import('../views/BookingDetailView.vue'),
+    },
+    {
+      path: '/bookings/update/:id',
+      name: 'booking-update',
+      component: () => import('../views/BookingUpdateView.vue'),
+    },
+    // Statistics routes
     {
       path: '/statistics',
       name: 'statistics',
